593b8642020209869e02efa7d52e25c13ce9fae4
Asmir Mustafic <goetas@lignano.it>
2013-04-08 15:24:13 +0200

    espansione parametri proxy ricursiva (secondo commit)

--- lib/Doctrine/ORM/Persisters/BasicEntityPersister.php
+++ lib/Doctrine/ORM/Persisters/BasicEntityPersister.php
@@ -1507,19 +1507,30 @@ class BasicEntityPersister
      */
     private function getIndividualValue($value)
     {
-        if (is_object($value) && $this->_em->getMetadataFactory()->hasMetadataFor(get_class($value))) {
-            if ($this->_em->getUnitOfWork()->getEntityState($value) === UnitOfWork::STATE_MANAGED) {
-                $idValues = $this->_em->getUnitOfWork()->getEntityIdentifier($value);
-            } else {
-                $class = $this->_em->getClassMetadata(get_class($value));
-                $idValues = $class->getIdentifierValues($value);
-            }
-            $value = reset($idValues);
-            if (is_object($value)){
-                $value = $this->getIndividualValue($value);
-            }
-        }
-
+    	if (is_object($value)){
+    		$className = get_class($value);
+    		$metadataFactory = $this->_em->getMetadataFactory();
+    		
+    		if ($value instanceof Proxy && !$metadataFactory->hasMetadataFor($className)) {
+    			$reflectionClass = new \ReflectionObject($value);
+    			if($reflectionClass->implementsInterface('Doctrine\ORM\Proxy\Proxy')) {
+    				$metadataFactory->setMetadataFor($className, $metadataFactory->getMetadataFor(ClassUtils::getRealClass($className)));
+    			}
+    		}
+	    	if ($metadataFactory->hasMetadataFor($className)) {
+	
+	            if ($this->_em->getUnitOfWork()->getEntityState($value) === UnitOfWork::STATE_MANAGED) {
+	                $idValues = $this->_em->getUnitOfWork()->getEntityIdentifier($value);
+	            } else {
+	                $class = $this->_em->getClassMetadata($className);
+	                $idValues = $class->getIdentifierValues($value);
+	            }
+	            $value = reset($idValues);
+	            if (is_object($value)){
+	                $value = $this->getIndividualValue($value);
+	            }
+	        }
+    	}
         return $value;
     }
 
